c97173c2b6168d281de18d33813863e4a3a95a86
Steve Chall <stevec@renci.org>
2017-03-07 15:56:18 -0500

    move code into fcn in preparation for processing by directory

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ add_codes_to_SSM.py accepts two arguments: the first is the path to an SSM JSON
 file (see https://github.com/steve9000gi/ssm) and the second is the path to the
 related CBLM file (see https://github.com/steve9000gi/AddCodesToBLM). It reads 
 in the two files, finds the code associated with each SSM node from the CBLM,
-adds the code to each node in the SSM as a new key/value pair, and writes the
-updated JSON object to a file.
+adds that code to the corresponding node in the SSM as a new key/value pair, and
+writes the updated JSON object to a file.
 </p>
--- add_codes_to_SSM.py
+++ add_codes_to_SSM.py
@@ -12,39 +12,40 @@ import json
 ssm = sys.argv[1]
 cblm = sys.argv[2]
 
-print 'ssm: ' + ssm + '; cblm: ' + cblm
+def add_codes_to_single_ssm(ssm, cblm):
+    with open(ssm) as json_input_file: # Read in ssm.json file
+	json_object = json.load(json_input_file)
 
-with open(ssm) as json_input_file: # Read in ssm.json file
-    json_object = json.load(json_input_file)
+    nodes = json_object["nodes"]
+    print 'ssm: ' + ssm + '; cblm: ' + cblm + "; # nodes:" , len(nodes)
 
-nodes = json_object["nodes"]
-print "type(nodes):" + type(nodes).__name__
-print "# nodes:" , len(nodes)
+    clist = []
 
-clist = []
+    with open(cblm) as cblm_file: # Read CBLM file as list, each item a row:
+	rows = cblm_file.read().split("\n");
 
-with open(cblm) as cblm_file: # Read CBLM file as list, each item a row:
-    rows = cblm_file.read().split("\n");
+    for row in rows: # clist becomes a list of sublists, each sublist a row
+	r = row.split("\t") # Split each row into a list of items
+	clist.append(r)
 
-for row in rows: # clist becomes a list of sublists, each sublist a row
-    r = row.split("\t") # Split each row into a list of items
-    clist.append(r)
+    clist.pop() # get rid of empty last row
 
-clist.pop() # get rid of empty last row
+    node_id_index = 1 # position for NodeID column in CBLM
+    code_index = 4    # position for Code column in CBLM
 
-node_id_index = 1 # position for NodeID column in CBLM
-code_index = 4    # position for Code column in CBLM
+    for c_node_line in clist[1:]: # Skipping headers, select each row
+	# Find which node in json object nodes has the same id  as the NodeID in
+	# the current c_node_line:
+	for j_ix, j_node in enumerate(nodes): # Look at each node from json object
+	    if str(j_node["id"]) == c_node_line[node_id_index]:
+		j_node["code"] = c_node_line[code_index] # Add matching code
+		break
 
-for c_node_line in clist[1:]: # Skipping headers, select each row
-    # Find which node in json object nodes has the same id  as the NodeID in
-    # the current c_node_line:
-    for j_ix, j_node in enumerate(nodes): # Look at each node from json object
-	if str(j_node["id"]) == c_node_line[node_id_index]:
-	    j_node["code"] = c_node_line[code_index] # Add matching code
-	    break
+    # Finally, write the json object with codes added to file:
+    outfilename = os.path.splitext(ssm)[0] + "-C.json"
+    print outfilename
+    with open(outfilename, "w") as outfile:
+	json.dump(json_object, outfile)
 
-# Finally, write the json object with codes added to file:
-outfilename = os.path.splitext(ssm)[0] + "-C.json"
-print outfilename
-with open(outfilename, "w") as outfile:
-    json.dump(json_object, outfile)
+# main
+add_codes_to_single_ssm(ssm, cblm)
